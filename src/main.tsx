893e68f94e1a4646666588320c6cef0f62577b37
Mariia Moiseienko <mariia.moiseienko1@gmail.com>
2025-12-02 14:46:32 +0100

    feat: add react-router, react-toolkit, storybook

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainRoutes from "./routes/MainRoutes";
+import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainRoutes />
+    },
+    {
+        path: "/product/:id",
+        element: <div>Product</div>
+    }
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+    <StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </StrictMode>,
+    );
+
+
+createRoot(document.getElementById('root')!)
