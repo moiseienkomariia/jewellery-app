83733c40c0f0f6c1e09e5113ea7870a4fcbc1a26
Mariia Moiseienko <mariia.moiseienko1@gmail.com>
2025-12-18 13:27:47 +0100

    add endpoints to api

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,5 +1,14 @@
 export const apiEndpoints = Object.freeze({
     categories: '/categories',
     products: '/products',
+    users: '/users',
+    orders: '/orders',
+    cart: '/cart',
+
+    getCategory: (id: string) => `/categories/${id}`,
+    getUser: (id: string) => `/user/${id}`,
     getProduct: (id: string) => `/products/${id}`,
+    getOrder: (id: string) => `/orders/${id}`,
+    getProductsByCategory: (id: string) => `categories/${id}/products`,
+    getProductByRating: (rating: number) => `/products?rating=${rating}`,
 })
